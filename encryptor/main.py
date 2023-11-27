b60103119c50ba3329a911023ec508bcb20f942f
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-11-27 20:41:09 +0100

    Add basic file and folder handling

--- encryptor/main.py
+++ encryptor/main.py
@@ -1,9 +1,18 @@
 import argparse
 import logging
-from os import getcwd
-from os import walk, listdir, makedirs
+import os
 from shutil import rmtree
 from my_secret import MySecret
+
+
+def dir_path(string):
+    logging.debug(f'STRING: {string}')
+    if os.path.exists(string):
+        return string
+    else:
+        #os.mkdir(string)
+        raise (NotADirectoryError(string))
+
 
 # INITIAL VALUES
 LVL_MAPPING = {
@@ -33,7 +42,7 @@ group_1.add_argument(
 group_2.add_argument(
     "--file",
     action="store",
-    type=str
+    type=dir_path
     )
 group_2.add_argument(
     "--folder",
@@ -62,24 +71,39 @@ print(args)
 secret = MySecret()
 if args.file:
     logging.debug("FILE")
-    print(getcwd()+"\\"+args.file)
+    target = "..\\result_file"
+    file = args.file.split("\\")
 
-else:
+    if os.path.exists(target):
+        rmtree(target)
+
+    os.mkdir(f'{target}')
+    with open(args.file, 'r', encoding='utf-8') as file_unsafe:
+        context = file_unsafe.read()
+        secret.safe_context = secret.encrypt_content(context)
+        logging.debug(f'Original content: {context}')
+        logging.debug(f'Safe content: {secret.safe_context}')
+
+    with open(f'{target}/{file[-1]}', 'w', encoding='utf-8') as file_safe:
+        file_safe.write(secret.safe_context)
+
+elif args.folder:
     logging.debug("FOLDER")
+    target = "..\\result_folder"
+    folder = args.folder.split("\\")
+    if os.path.exists(target):
+        rmtree(target)
 
+    for path, directories, files in os.walk(args.folder):
+        if path.startswith(".."):
+            new_path = path[2:]
+        os.makedirs(f'{target}/{new_path}')
+        for file in files:
+            with open(f'{path}/{file}', 'r', encoding='utf-8') as file_unsafe:
+                context = file_unsafe.read()
+                secret.safe_context = secret.encrypt_content(context)
+                logging.debug(f'Original content: {context}')
+                logging.debug(f'Safe content: {secret.safe_context}')
 
-target = "result"
-if target in listdir('.'):
-    rmtree(target)
-
-for path, directories, files in walk('Poland'):
-    makedirs(f'{target}/{path}')
-    for file in files:
-        with open(f'{path}/{file}', 'r', encoding='utf-8') as file_unsafe:
-            context = file_unsafe.read()
-            secret.safe_context = secret.encrypt_content(context)
-            logging.debug(f'Original content: {context}')
-            logging.debug(f'Safe content: {secret.safe_context}')
-
-        with open(f'{target}/{path}/{file}', 'w', encoding='utf-8') as file_safe:
-            file_safe.write(secret.safe_context)
+            with open(f'{target}/{new_path}/{file}', 'w', encoding='utf-8') as file_safe:
+                file_safe.write(secret.safe_context)
