b9f0de46b740cdc354d2e14445f8312ea24f0530
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-12-04 22:27:31 +0100

    Add verbose and refactor file / directory selector

--- encryptor/main.py
+++ encryptor/main.py
@@ -2,8 +2,59 @@ import argparse
 import logging
 import os
 import pathlib
+from time import time
+from tqdm import tqdm
 from cryptography.fernet import InvalidToken
 from crypto import Encrypt, Decrypt
+
+
+def main(args):
+    # MAIN PROGRAM
+    start = time()
+    try:
+        # FILE / FILES
+        if args.file:
+
+            files_to_process = args.file
+            if args.verbose == 3:
+                files_to_process = tqdm(args.file)
+            for file in files_to_process:
+                file = pathlib.Path(file)
+
+                if args.mode == "encrypt":
+                    action = Encrypt(file)
+                elif args.mode == "decrypt":
+                    action = Decrypt(file)
+
+                action.execute(args.password)
+                stop = time()
+                process_time = stop - start
+                verbose(file, process_time, files_to_process)
+
+        # DIRECTORY
+        elif args.folder:
+            # TODO:
+            pass
+
+    except InvalidToken:
+        print("INVALID TOKEN")
+
+
+def verbose(file, process_time, files_to_process):
+    # VERBOSE
+    if args.verbose == 1 or args.verbose <= 2:
+        # VERBOSE LEVEL 1
+        print(f'File: {file.name}', end='')
+
+        if args.verbose == 2:
+            # VERBOSE LEVEL 2
+            print(f' Processed in: {process_time} sec', end='')
+
+        print()
+
+    if args.verbose >= 3:
+        # VERBOSE LEVEL 3
+        files_to_process.set_description(file.name)
 
 
 def dir_path(string) -> str:
@@ -24,21 +75,22 @@ def dir_path(string) -> str:
     return string
 
 
-# INITIAL VALUES
-LVL_MAPPING = {
-    0: 50,  # CRITICAL
-    1: 40,  # ERROR
-    2: 20,  # INFO
-    3: 10   # DEBUG
-}
+# INIT SAMPLE FOLDERS
+START_FOLDERS = ['sample_file', 'sample_folder']
 
+for location in START_FOLDERS:
+    location = '..\\' + location
+    if not os.path.exists(location):
+        os.mkdir(location)
+
+# INIT DATA FROM CONSOLE
 parser = argparse.ArgumentParser(description='Decrypt encrypt APP')
 group_1 = parser.add_mutually_exclusive_group()
 group_2 = parser.add_mutually_exclusive_group()
 
 group_1.add_argument(
     "-v",
-    "-verbose",
+    "--verbose",
     action='count',
     default=0,
     help="available verbose level (1,2,3)"
@@ -52,8 +104,8 @@ group_1.add_argument(
 group_2.add_argument(
     "-f",
     "--file",
-    action="store",
-    type=dir_path
+    action="append",
+    type=str
     )
 group_2.add_argument(
     "-d",
@@ -77,57 +129,5 @@ parser.add_argument(
     )
 
 args = parser.parse_args()
-logging.basicConfig(format='%(message)s')
-logging.getLogger().setLevel(LVL_MAPPING[args.v])
-logging.debug("Input args : %s", args)
 
-# INIT SAMPLE FOLDERS
-START_FOLDERS = ['sample_file', 'sample_folder']
-
-for location in START_FOLDERS:
-    location = '..\\' + location
-    if not os.path.exists(location):
-        os.mkdir(location)
-
-# MAIN PROGRAM
-try:
-    # ENCRYPT
-    if args.mode == "encrypt":
-        logging.debug("ENCRYPT")
-
-        # FILE / FILES
-        if args.file:
-            path = pathlib.Path(args.file)
-            logging.debug("FILE")
-            file = Encrypt(path)
-            file.execute(args.password)
-
-            logging.debug("OK")
-
-        # DIRECTORY
-        elif args.folder:
-            logging.debug("DIRECTORY")
-            # TODO:
-            # target = "..\\result_folder"
-
-    # DECRYPT
-    elif args.mode == "decrypt":
-        logging.debug("DECRYPT")
-
-        # FILE / FILES
-        if args.file:
-            path = pathlib.Path(args.file)
-            logging.debug("FILE")
-            file = Decrypt(path)
-            file.execute(args.password)
-
-            logging.debug("OK")
-
-        # DIRECTORY
-        elif args.folder:
-            logging.debug("DIRECTORY")
-            # TODO:
-            # target = "..\\result_folder"
-
-except InvalidToken:
-    logging.error('INVALID TOKEN')
+main(args)
